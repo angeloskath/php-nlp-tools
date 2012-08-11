fffd64e67cee34eb5cfe4e2aeddfdd6c17f5d0b8
Angelos Katharopoulos <katharas@gmail.com>
2012-08-11 20:42:35 +0300

    WhitespaceTokenizer name fix
    I fixed the WhitespaceTokenizer to split only on whitespace
    because it split also on punctuation characters.
    I added a WhitespaceAndPunctuationTokenizer for this.

--- tokenizer/whitespace_punctuation_tokenizer.php
+++ tokenizer/whitespace_punctuation_tokenizer.php
@@ -5,12 +5,14 @@ namespace NlpTools;
 /*
  * Simple white space tokenizer. Breaks either on whitespace or on word
  * boundaries (ex.: dots, commas, etc)
+ * Does not include white space in tokens.
+ * Every punctuation character is a signle token
  */
-class WhitespaceTokenizer implements Tokenizer
+class WhitespaceAndPunctuationTokenizer implements Tokenizer
 {
 	public function tokenize($str) {
 		$arr = array();
-		preg_match_all('/(\s)*([^\pP\s]+|.)(\s)*/u',$str,$arr);
+		preg_match_all('/(\s*)([^\pP\s]+|.)(\s*)/u',$str,$arr);
 		return $arr[2];
 	}
 }
---
+++ tokenizer/whitespace_tokenizer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace NlpTools;
+
+/*
+ * Simple white space tokenizer.
+ * Break on every white space
+ */
+class WhitespaceTokenizer implements Tokenizer
+{
+	public function tokenize($str) {
+		$arr = array();
+		return preg_split('/\s+/',$str,null,PREG_SPLIT_NO_EMPTY);
+	}
+}
+
+?>
