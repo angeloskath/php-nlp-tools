836083fc7cc11b3cc9187627eaac60e9b8b1c679
Dan Cardin <dcardin2007@gmail.com>
2013-09-24 08:03:04 -0400

    fix for issue #16 penntreebank tokenizer is not implemented correctly

--- tests/NlpTools/Tokenizers/PennTreeBankTokenizerTest.php
+++ tests/NlpTools/Tokenizers/PennTreeBankTokenizerTest.php
@@ -8,17 +8,24 @@ namespace NlpTools\Tokenizers;
  */
 class PennBankTokenizerTest extends \PHPUnit_Framework_TestCase
 {
-
+    
     public function testTokenizer()
     {
         $tokenizer = new PennTreeBankTokenizer();
-        $this->assertCount(14, $tokenizer->tokenize("Good muffins cost $3.88\nin New York.  Please buy me\ntwo of them.\nThanks."));
+        $tokens = $tokenizer->tokenize("Good muffins cost $3.88\nin New York.  Please buy me\ntwo of them.\nThanks.");
+        $this->assertCount(16, $tokens);
     }
 
     public function testTokenizer2()
     {
         $tokenizer = new PennTreeBankTokenizer();
-        $this->assertCount(6, $tokenizer->tokenize("They'll save and invest more."));
+        $this->assertCount(7, $tokenizer->tokenize("They'll save and invest more."));
     }
+    
+    public function testTokenizer3()
+    {
+        $tokenizer = new PennTreeBankTokenizer();
+        $this->assertCount(4, $tokenizer->tokenize("I'm some text"));
+    }    
 
 }
