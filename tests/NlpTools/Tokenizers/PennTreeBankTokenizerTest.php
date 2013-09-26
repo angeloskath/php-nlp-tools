7943779cde67da28c4ab359fd21fc90dbde5d626
Dan Cardin <dcardin2007@gmail.com>
2013-09-26 09:57:51 -0400

    Implementing TokenTransformationInterface

--- tests/NlpTools/Tokenizers/PennTreeBankTokenizerTest.php
+++ tests/NlpTools/Tokenizers/PennTreeBankTokenizerTest.php
@@ -26,6 +26,26 @@ class PennBankTokenizerTest extends \PHPUnit_Framework_TestCase
     {
         $tokenizer = new PennTreeBankTokenizer();
         $this->assertCount(4, $tokenizer->tokenize("I'm some text"));
-    }    
+    }
+    
+    public function testComparison()
+    {
+        $this->markTestIncomplete(
+          'This test is not complete, because it does not work to compare the outputs, because the underlying tokenizers differ in behavior.'
+        );
+        //test data is based on the same test file, the tokens are unique and lower case
+        $testWordSet = explode(PHP_EOL, file_get_contents(TEST_DATA_DIR.'/Tokenizers/PennTreeBankTokenizerTest/nltk_output.txt'));
+            
+        $tokenizer = new PennTreeBankTokenizer();
+        //text is normalized to lower case
+        $tokens = $tokenizer->tokenize(strtolower(file_get_contents(TEST_DATA_DIR.'/Tokenizers/PennTreeBankTokenizerTest/test.txt')));
+        $tokens = array_unique($tokens);
+        sort($tokens);
+        
+        foreach($testWordSet as $testWord){ 
+            $this->assertTrue(in_array($testWord, $tokens), "The tokenized word '$testWord' was not found.");
+        }
+                
+    }
 
 }
