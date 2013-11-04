774ee18ca6137d29c85e6ec17b4616447b022dfe
Angelos Katharopoulos <katharas@gmail.com>
2013-11-04 15:00:25 +0200

    Add an implementation of G. Ntais stemmer
    TODO:
     - Add tests

---
+++ tests/NlpTools/Stemmers/GreekStemmerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace NlpTools\Stemmers;
+
+class GreekStemmerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test the words found in Appendix A from Mr. Ntais's thesis.
+     */
+    public function testFromAppendixA()
+    {
+        $this->fail("This test is not yet implemented");
+    }
+}
