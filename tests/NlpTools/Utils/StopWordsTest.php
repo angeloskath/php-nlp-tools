efa0fd32e1e6ab39c82ba3851496e32d31365988
Julien Malige <julienmalige@gmail.com>
2016-02-29 11:59:27 -0300

    Fix issue #37, stop words - optional transformation interface

--- tests/NlpTools/Utils/StopWordsTest.php
+++ tests/NlpTools/Utils/StopWordsTest.php
@@ -3,6 +3,7 @@
 namespace NlpTools\Utils;
 
 use NlpTools\Documents\TokensDocument;
+use NlpTools\Utils\Normalizers\Normalizer;
 
 class StopWordsTest extends \PHPUnit_Framework_TestCase
 {
@@ -16,6 +17,26 @@ class StopWordsTest extends \PHPUnit_Framework_TestCase
         );
 
         $doc = new TokensDocument(explode(" ","if you tell the truth you do not have to remember anything"));
+        $doc->applyTransformation($stopwords);
+        $this->assertEquals(
+            array(
+                "if", "you", "tell", "truth", "you", "do", "not", "have", "remember", "anything"
+            ),
+            $doc->getDocumentData()
+        );
+    }
+
+    public function testStopwordsWithTransformation()
+    {
+        $stopwords = new StopWords(
+            array(
+                "to",
+                "the"
+            ),
+            Normalizer::factory("English")
+        );
+
+        $doc = new TokensDocument(explode(" ", "if you tell The truth you do not have To remember anything"));
         $doc->applyTransformation($stopwords);
         $this->assertEquals(
             array(
