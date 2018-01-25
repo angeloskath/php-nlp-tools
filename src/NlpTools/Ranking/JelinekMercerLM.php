2d60783df01fd1d9d762ca7976f729fc38b81ffd
jtejido <jtbibliomania@gmail.com>
2018-01-25 10:00:55 +0800

    changed μ in DirichletLM to match the optimal value. added JelinekMercerLM.

--- src/NlpTools/Ranking/JelinekMercerLM.php
+++ src/NlpTools/Ranking/JelinekMercerLM.php
@@ -7,23 +7,23 @@ use NlpTools\Ranking\ScoringInterface;
 
 
 /**
- * DirichletLM is a class for ranking documents against a query based on Bayesian smoothing with 
+ * JelinekMercerLM is a class for ranking documents against a query based on Bayesian smoothing with 
  * Dirichlet Prior for language modelling.
  *
  * From Chengxiang Zhai and John Lafferty. 2001. A study of smoothing methods for language models applied
  * to Ad Hoc information retrieval. In Proceedings of the 24th annual international ACM SIGIR conference on 
  * Research and development in information retrieval (SIGIR '01).
  * http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.94.8019&rep=rep1&type=pdf
- * The optimal for μ appears to have a wide range (500-10000) and usually is around 2000.
+ * The value for λ is generally very small (0.1) for title queries and around 0.7 for verbose.
  *
  * @author Jericko Tejido <jtbibliomania@gmail.com>
  */
 
 
-class DirichletLM implements ScoringInterface
+class JelinekMercerLM implements ScoringInterface
 {
 
-    const C = 2000;
+    const C = 0.7;
 
     protected $math;
 
@@ -45,7 +45,7 @@ class DirichletLM implements ScoringInterface
         $score = 0;
 
         if($tf != 0){
-            $score += $this->math->mathLog(1 + ($tf/($this->c * ($termFrequency / $collectionLength))) ) + $this->math->mathLog($this->c/($docLength+$this->c));
+            $score += $this->math->mathLog(1 + $tf / ($this->c * ($termFrequency / $collectionLength))) +        $this->math->mathLog($this->c / ($docLength + $this->c));
         }
 
         return $score;
